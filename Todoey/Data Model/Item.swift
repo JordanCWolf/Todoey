9c4c98fcc0ec303e5d0037bfda1a8459dc946050
Jordan Wolf <j_wolf23@yahoo.com>
2019-03-19 16:40:43 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jordan Wolf on 3/18/19.
+//  Copyright © 2019 Jordan Wolf. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
